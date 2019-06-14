75024c23a32459ef92961facbd8d54ce9afd78b2
Nate Snapp <nate.snapp@gmail.com>
2019-06-14 00:54:11 -0700

    About page with attribution and deduped content

--- about.vue
+++ about.vue
@@ -1,22 +1,21 @@
 <template lang="md">
-  Nate Snapp is a Big Data and Reliability Architect focused on Hadoop, Spark, Kafka as well 
-  as pushing into areas of Machine Learning and AI with toolkits such as Tensorflow and PyTorch.
-  As a person focused on all things that make software robust - currently doing his most 
+  Nate Snapp is a Big Data and Reliability Architect focused on Hadoop, Spark, Kafka, Elastic Search 
+  as well as pushing into areas of Machine Learning and AI with toolkits such as Tensorflow and 
+  PyTorch.  As a person focused on all things that make software robust - currently doing his most 
   productive work in Java, Python, Kotlin, and Gradle.
-  <figure>
-    <img src="/img/responsive-workstation.jpg"/>
-    <figcaption>Where the magic happens.</figcaption>
+  <figure class="small center">
+    <img src="/img/coding-at-workstation.jpg"/>
   </figure>
 
-  More information on his skills and experience can be found at his
-  [LinkedIn page](https://www.linkedin.com/in/natesnapp).
+  More information about Nate's skills and experience can be found on 
+  [LinkedIn](https://www.linkedin.com/in/natesnapp).
 
   ### About This Site
 
-  As an open-source software contributor and consumer, as well as appreciator of free services; here's a few notes
-  concerning the making of this website:
+  This site uses services and technology developed by [Rich Snapp](https://www.richsnapp.com), 
+  and is based on open-source:
 
-  * This is a [static web page](https://en.wikipedia.org/wiki/Static_web_page) generated with
+  * Site is a [static web page](https://en.wikipedia.org/wiki/Static_web_page) generated with
     [Nuxt.js](https://github.com/nuxt/nuxt.js/) and various other npm libraries.
   * Hosted with [Netlify](https://www.netlify.com/)
   * Using some [Twitter Bootstrap](https://getbootstrap.com/) components (such as the grid) and
@@ -25,5 +24,5 @@
   * Share functionality by [ShareThis](https://www.sharethis.com).
   * Type fonts by [Google Fonts](https://www.google.com/fonts).
   * Comments using the [Facebook Social Comments](https://developers.facebook.com/products/social-plugins/comments/) plugin.
-  * Intentionally made as simple and unparallaxy as possible.
+  * Focus is on readability and intentionally avoiding trendy embellishments (Parallax scrolling, unnecessary javascript-based animation).
 </template>
